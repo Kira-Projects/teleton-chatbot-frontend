51089f1460959c2061b9d03e46eee83a3683c585
Leandro Code <154637101+leandro-codee@users.noreply.github.com>
2025-04-24 10:30:27 -0400

    Merge pull request #1 from Kira-Projects/feature/add-admin-page
    Feature/add admin page

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,20 @@ import { Provider } from "react-redux"
 import store from "./store/store"
 import { BrowserRouter, Route } from "react-router-dom"
 import RoutesWithNotFound from "./components/RoutesWithNotFound"
-import Home from "./pages/public/Home.tsx";
+import Home from "./pages/public/Home.tsx"
+import Admin from "./pages/admin/Admin.tsx"
 
 const App = () => {
-    return (
-        <Provider store={store}>
-            <BrowserRouter>
-                <RoutesWithNotFound>
-                    <Route path="/" element={<Home />} />
-                </RoutesWithNotFound>
-            </BrowserRouter>
-        </Provider>
-    )
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <RoutesWithNotFound>
+          <Route path="/" element={<Home />} />
+          <Route path="/admin" element={<Admin />} />
+        </RoutesWithNotFound>
+      </BrowserRouter>
+    </Provider>
+  )
 }
 
 export default App
